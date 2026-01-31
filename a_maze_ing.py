05acf8d2c34b0bb04f5d2dacdd18e995f3aecad7
Seddik Hallil <sehallil@e2r9p11.1337.ma>
2026-01-31 21:14:27 +0100

    parsed coodrdination from config.txt file

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,14 +1,21 @@
 import curses
 from curses import wrapper
-from sys import argv
 from cells_place_holder import MazeGenerator
 from utils import Maze
 
-if len(argv) < 3:
-    print("Usage: python3 a_maze_ing.py <width> <height>")
-    exit(1)
-
-WIDTH, HEIGHT = int(argv[1]), int(argv[2])
+config = {}
+try:
+    with open("config.txt", "r") as fd:
+        for line in fd:
+            if "=" in line and not line.startswith("#"):
+                key, value = line.strip().split("=", 1)
+                config[key.strip().upper()] = value.strip()
+        WIDTH = int(config.get("WIDTH", 10))
+        HEIGHT = int(config.get("HEIGHT", 10))
+except (FileNotFoundError, PermissionError, KeyError, ValueError) as e:
+    print(f"[Error loading], using default config {e}")
+    WIDTH = 10
+    HEIGHT = 10
 
 
 def render_elements(stdscr, elements):
@@ -34,7 +41,7 @@ def main(stdscr: curses.window) -> None:
     maze: Maze = Maze(
         generator.generate(),
         HEIGHT, WIDTH,
-        int((term_width / 2) - WIDTH),
+        int((term_width / 2) - (WIDTH / 2)),
         int((term_height / 2) - (HEIGHT / 2)),
     )
 
